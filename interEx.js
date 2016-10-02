6ccfa0b18da6c515376255df3be2bf3967b8b1ce
Aman Gupta <amangupta96@outlook.com>
2016-10-02 09:07:27 +0800

    Added dynamic symbol stuff

--- interEx.js
+++ interEx.js
@@ -8,26 +8,26 @@ var requestToApi = require('./functions.js').requestToApi;
 const TEAM_UID = "TtH8CwcTEcwcpP7BOoZBzg";
 var symbol = "0005";
 
-function main(callback){
-		getAllFunctions.getAllMarketDataSync('0388', function(data){
+function main(symbol, callback) {
+	getAllFunctions.getAllMarketDataSync(symbol, function (data) {
 		finalMinMax = [];
-		getAllFunctions.getMinSync(data, function(minData){
+		getAllFunctions.getMinSync(data, function (minData) {
 			//console.log("Buy", minData);
-			var test = _.minBy(minData, function(o) { return 'o.buy.price1'; });
+			var test = _.minBy(minData, function (o) { return 'o.buy.price1'; });
 			finalMinMax.push(test);
 		});
-		getAllFunctions.getMaxSync(data, function(maxData){
+		getAllFunctions.getMaxSync(data, function (maxData) {
 			//console.log("Sell", maxData);
-			var test = _.maxBy(maxData, function(o) { return 'o.sell.price1'; });
+			var test = _.maxBy(maxData, function (o) { return 'o.sell.price1'; });
 			finalMinMax.push(test);
 		});
 		callback(finalMinMax);
 	});
 }
 
-(function repeat(){
-	main(function(res)
-	{	
+(function repeat(symbol) {
+	// console.log(symbol)
+	main(symbol, function (res) {
 		console.log(res);
 		var minQ = res[0].buy.qty1;
 		var maxQ = res[1].sell.qty1;
@@ -41,45 +41,45 @@ function main(callback){
 		}else{
 			var quantity = maxQ;
 		}
-		
-		if((maxPrice) < (minPrice) && quantity != null && quantity > 0){
-				console.log("Buy "+quantity+ " at $"+minPrice+" from exchange "+ buyEx +" and sell to " + sellEx+" sell at $" + maxPrice );
 
-				 var output = requestToApi({
-					'apiCall':'orders',
-					'symbol': symbol,
-					'exchange': 'exchange'+buyEx,
-					'orderTicket': {"side": "buy",
-									"qty":quantity,
-									"order_type":"market"}
-				});
-				
-				console.log(output);
-				
-				/* var buy = 0;
-				_.forEach(output.fills, function(value) {
-					buy = buy + (value.price * value.qty);
-				}); */
-				 
-			
-				 var output2 = requestToApi({
-					'apiCall':'orders',
-					'symbol': symbol,
-					'exchange': 'exchange'+sellEx,
-					'orderTicket': {"side": "sell",
-									"qty":quantity,
-									"order_type":"market"}
-				});
-				
-				console.log(output2);
-				/* 	var sell = 0;
-				_.forEach(output2.fills, function(value) {
-					sell = sell + (value.price * value.qty);
-				});
-				
-				console.log("Profit:" + (sell - buy)); */
-		}
-		repeat();
-			
+		// if((maxPrice) < (minPrice) && quantity != null && quantity > 0){
+		// 		console.log("Buy "+quantity+ " at $"+minPrice+" from exchange "+ buyEx +" and sell to " + sellEx+" sell at $" + maxPrice );
+
+		// 		//  var output = requestToApi({
+		// 		// 	'apiCall':'orders',
+		// 		// 	'symbol': symbol,
+		// 		// 	'exchange': 'exchange'+buyEx,
+		// 		// 	'orderTicket': {"side": "buy",
+		// 		// 					"qty":quantity,
+		// 		// 					"order_type":"market"}
+		// 		// });
+
+		// 		// console.log(output);
+
+		// 		// /* var buy = 0;
+		// 		// _.forEach(output.fills, function(value) {
+		// 		// 	buy = buy + (value.price * value.qty);
+		// 		// }); */
+
+
+		// 		//  var output2 = requestToApi({
+		// 		// 	'apiCall':'orders',
+		// 		// 	'symbol': symbol,
+		// 		// 	'exchange': 'exchange'+sellEx,
+		// 		// 	'orderTicket': {"side": "sell",
+		// 		// 					"qty":quantity,
+		// 		// 					"order_type":"market"}
+		// 		// });
+
+		// 		// console.log(output2);
+		// 		/* 	var sell = 0;
+		// 		_.forEach(output2.fills, function(value) {
+		// 			sell = sell + (value.price * value.qty);
+		// 		});
+
+		// 		console.log("Profit:" + (sell - buy)); */
+		// }
+		repeat('0005');
 	});
-})();
+})('0005');
+
